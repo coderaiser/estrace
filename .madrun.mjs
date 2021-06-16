20b65a4bd6c67e1e4689af548ef8bbf20a5c7db9
coderaiser <coderaiser@cloudcmd.io>
2021-06-16 16:26:56 +0300

    feature(estrace) add file paths

--- .madrun.mjs
+++ .madrun.mjs
@@ -4,13 +4,14 @@ import {
 } from 'madrun';
 
 const NODE_OPTIONS = `'--loader ./lib/estrace.js'`;
-const testEnv = {
+const traceEnv = {
     NODE_OPTIONS,
 };
 
 export default {
-    'test': () => [testEnv, `tape 'test/**/*.js' 'lib/**/*.spec.js'`],
-    'coverage': async () => [testEnv, `c8 --exclude="lib/**/{fixture,*.spec.js}" ${await cutEnv('test')}`],
+    'test': () => `tape 'test/**/*.js' 'lib/**/*.spec.js'`,
+    'trace:test': () => [traceEnv, `tape 'test/**/*.js' 'lib/**/*.spec.js'`],
+    'coverage': async () => `c8 --exclude="lib/**/{fixture,*.spec.js}" ${await cutEnv('test')}`,
     'lint': () => 'putout .',
     'fresh:lint': () => run('lint', '--fresh'),
     'lint:fresh': () => run('lint', '--fresh'),
@@ -18,11 +19,11 @@ export default {
     'report': () => 'c8 report --reporter=lcov',
     'watcher': () => 'nodemon -w test -w lib --exec',
     
-    'watch:test': async () => await run('watcher', `"${await cutEnv('test')}"`, testEnv),
+    'watch:test': async () => await run('watcher', `"${await cutEnv('test')}"`),
     
     'watch:lint': () => run('watcher', '\'npm run lint\''),
     'watch:tape': () => 'nodemon -w test -w lib --exec tape',
     
-    'watch:coverage': async () => await run('watcher', await cutEnv('coverage'), testEnv),
+    'watch:coverage': async () => await run('watcher', await run('coverage')),
 };
 
